f1147673b7bac6cc8b7a8f00642754d01545e8a1
354073823@qq.com <354073823@qq.com>
2023-01-19 00:56:23 +0800

    commit: 提交一个react项目

--- node/test.js
+++ node/test.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-const arr = [9, 2, 3]
-
-console.log(arr.shift());
-
-console.log(arr);
-=======
 Promise.resolve().then(() => { 
  console.log(0); 
  return 4
@@ -26,4 +19,3 @@ Promise.resolve().then(() => {
 }).then(() =>{ 
  console.log(6); 
 }) 
->>>>>>> fb895d37b6e6bef6cc30d44c1bd1f45c553f374a
